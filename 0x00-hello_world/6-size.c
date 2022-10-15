28ffcb123e9093c99fee0370f282df0c9010e682
MurphyLomboli <murphylomboli@outlook.com>
2022-10-15 05:59:52 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,11 +5,11 @@
  */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float e;
+	char = a;
+	int = b;
+	long int = c;
+	long long int = d;
+	float = e;
 
 	printf("Size of a char: %d byte(s)\n", sizeof(a));
 	printf("Size of an int: %d byte(s)\n", sizeof(b));
