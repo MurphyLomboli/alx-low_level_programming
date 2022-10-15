48e70f42ca8593ab00b359e65b469815504e5c08
MurphyLomboli <murphylomboli@outlook.com>
2022-10-15 05:19:53 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - Entry
- * return: Always 0
+ * return: Always 0 (Successful)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle, \n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
