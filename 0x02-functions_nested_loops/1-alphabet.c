af2757a0e93cd0b6ee8ff67cf38064068332edab
MurphyLomboli <murphylomboli@outlook.com>
2022-10-18 22:45:29 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,11 +6,10 @@
 
 void print_alphabet(void)
 {
-	int j;
+	char la;
 
-	for (j = 'a'; j <= 'z'; j++)
-	{
-		_putchar(j);
-	}
-	_putchar(j);
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar(la);
+
+	_putchar('\n');
 }
