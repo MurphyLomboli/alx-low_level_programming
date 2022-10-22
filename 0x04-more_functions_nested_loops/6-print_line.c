00293fde4e5cc1bedb66830355f055d99fb28b0c
MurphyLomboli <murphylomboli@outlook.com>
2022-10-22 00:02:30 -0700

    Task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,19 +1,18 @@
 #include "main.h"
 
-
 /**
  * print_line -> printing line
  * @n: integer params
  */
 void print_line(int n);
 {
-	int x;
+	int X;
 
 	if (n <= 0)
 		_putchar('\n');
 	else
 	{
-		for (x = 0; x < n; x++)
+		for (X = 0; X < n; X++)
 			_putchar(95);
 		_putchar('\n');
 
