4b2df5461cad85243e7d3b9142a00742a6dd2f59
MurphyLomboli <murphylomboli@outlook.com>
2022-11-02 09:52:35 -0700

    Task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,16 +1,27 @@
 #include "main.h"
 
 /**
- * is_divisible - checks if a number is divisible
- * @num: The number to be checked
- * @div: The divisor
- * Return: On success 0
- * On error, 1 is then returned, otherwise the errno is set accordingly
+ * sqrt2 - Makes it possible to evaluate from 1 to n
+ * @a: same number as n
+ * @b: number that iterates from 1 to n
+ * Return: On success 1
+ * On error, 0 is then returned, otherwise the errno is set accordingly
  */
-int is_divisible(int num, int div);
-int is_prime_number(int n);
-int is_divisible(int num, int div)
+int sqrt2(int a, int b)
 {
-	if (num % div == 0)
+	if (b * b == a)
+		return (1);
+	else if (b * b > a)
 		return (0);
+	return (sqrt2(a, b + 1));
+}
+/**
+ * is_prime_number - returns 1 if the input integer is a prime number
+ * @n: The number integer
+ * Return: On success 1
+ * On error, -1 is then returned, otherwise the errno is set accordingly
+ */
+int is_prime_number(int n)
+{
+	return (sqrt2(n, 1));
 }
