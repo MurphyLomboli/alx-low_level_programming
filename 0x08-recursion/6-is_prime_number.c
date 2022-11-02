193959c995d1669b58dbf526e52573879747bc3e
MurphyLomboli <murphylomboli@outlook.com>
2022-11-02 09:00:12 -0700

    Task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -5,23 +5,23 @@
  * @a: same number as n
  * @b: number that iterates from 1 to n
  * Return: On success 1
- * On error, -1 is then returned, otherwise the errno is set accordingly
+ * On error, 0 is then returned, otherwise the errno is set accordingly
  */
 int sqrt2(int a, int b)
 {
 	if (b * b == a)
 		return (b);
 	else if (b * b > a)
-		return (-1);
+		return (0);
 	return (sqrt2(a, b + 1));
 }
 /**
- * _sqrt_recursion - returns the natural square of n
+ * is_prime_number - returns the natural square of n
  * @n: The number integer
  * Return: On success 1
- * On error, -1 is then returned, otherwise the errno is set accordingly
+ * On error, 0 is then returned, otherwise the errno is set accordingly
  */
-int _sqrt_recursion(int n)
+int is_prime_number(int n)
 {
 	return (sqrt2(n, 1));
 }
