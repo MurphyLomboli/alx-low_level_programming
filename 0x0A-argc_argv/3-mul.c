f6c9a2cbd6319ce6db8baa23f68118d81db8ae6a
MurphyLomboli <murphylomboli@outlook.com>
2022-11-04 08:33:16 -0700

    Task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib>
+
+/**
+ * main - prints the program name
+ * @argc: counts the arguments
+ * @argv: the arguments
+ * Return: 0 if true, and 1 if false
+ */
+int main(int argc, char *argv[])
+{
+	int a, b;
+
+	if (argc == 3)
+
+	{
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		printf("%d\n", a * b);
+		return (0);
+	}
+
+	printf("Error/n");
+
+	return (1);
+}
